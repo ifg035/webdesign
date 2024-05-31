5d6777a1dcc4cf8b553e12f55b524c946deb36b6
임예은 <ifg.035@daum.net>
2024-05-31 16:39:50 +0900

    Asite

--- 실전문제/siteA/script/script.js
+++ 실전문제/siteA/script/script.js
@@ -1,4 +1,16 @@
 $(function(){
+    //slider
+    let currentIndex = 0;
+    $(".slider").hide().first().show();
+
+    setInterval(function(){
+        let nextIndex =(currentIndex + 1) % 3;
+        $(".slider").eq(currentIndex).fadeOut(1200);
+        $(".slider").eq(nextIndex).fadeIn(1200);
+
+        currentIndex = nextIndex ;
+    },3000)
+
     //nav
     $(".nav > ul > li").mouseover()
 });
